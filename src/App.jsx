0ebef3299d1f76c22246edf3f588394fcf43f294
Valeria-CPaz <val.paz@gmail.com>
2026-04-10 10:49:41 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import NavBar from "#components/NavBar";
+
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">Yeyy</h1></div>
+    <main>
+      <NavBar/>
+    </main>
   )
 }
 
